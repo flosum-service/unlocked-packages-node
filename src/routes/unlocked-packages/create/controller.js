2c16b4eb17c29093161702d4ae719a5887e4b124
d_karpenko <dmitry.karpenko@succraft.com>
2020-11-02 12:57:58 +0300

    Added test mode.
    Without creation unlocked package.

--- src/routes/unlocked-packages/create/controller.js
+++ src/routes/unlocked-packages/create/controller.js
@@ -2,6 +2,40 @@ const fs = require('fs');
 const helper = require('./helper');
 const constants = require('../../../constants');
 
+function devMode(body, projectName, resBody, log) {
+  return Promise.resolve()
+    .then(() => helper.callChildProcess(
+      constants.getSFDXCreateUnlockedPackage(body.packageName, body.username, body.description),
+      log,
+      { cwd: `./${projectName}`, maxBuffer: 1024 * 500 },
+      true,
+    ))
+    .then((result) => {
+      if (result === constants.PACKAGE_WITH_THIS_NAME_IS_EXIST) {
+        log.log(constants.PACKAGE_WITH_THIS_NAME_IS_EXIST);
+        return helper.addExistProjectToSFDXProject(projectName, body.packageName, log);
+      }
+      log.log('SFDX Unlocked Package Created');
+      log.log(result.stdout);
+      return Promise.resolve();
+    })
+    .then(() => helper.callChildProcess(
+      constants.getSFDXCreateUnlockedPackageVersion(body.packageName, body.username, body.versionKey, body.versionName, body.description, body.versionNumber),
+      log,
+      { cwd: `./${projectName}`, maxBuffer: 1024 * 500 },
+    ))
+    .then((stdout) => {
+      stdout = ' Request in progress. Sleeping 30 seconds. Will wait a total of 600 more seconds before timing out. Current Status=\'Initializing\'\n Request in progress. Sleeping 30 seconds. Will wait a total of 570 more seconds before timing out. Current Status=\'Verifying metadata\'\n sfdx-project.json has been updated.\n Successfully created the package version [08c2w000000k9zUAAQ]. Subscriber Package Version Id: 04t2w000009F4ILAA0\n Package Installation URL: https://login.salesforce.com/packaging/installPackage.apexp?p0=04t2w000009F4ILAA0\n As an alternative, you can use the "sfdx force:package:install" command.';
+      log.log('SFDX Unlocked Package Version Created');
+      log.log(stdout);
+      return helper.getInstallationURL(stdout);
+    })
+    .then((installationURL) => {
+      resBody.installationURL = installationURL;
+      return Promise.resolve();
+    });
+}
+
 function createUnlockedPackage(body, log) {
   const resBody = {};
   return new Promise((resolve, reject) => {
@@ -25,34 +59,12 @@ function createUnlockedPackage(body, log) {
         log,
         { cwd: `./${projectName}`, maxBuffer: 1024 * 500 },
       ))
-      .then(() => helper.callChildProcess(
-        constants.getSFDXCreateUnlockedPackage(body.packageName, body.username, body.description),
-        log,
-        { cwd: `./${projectName}`, maxBuffer: 1024 * 500 },
-        true,
-      ))
-      .then((result) => {
-        if (result === constants.PACKAGE_WITH_THIS_NAME_IS_EXIST) {
-          log.log(constants.PACKAGE_WITH_THIS_NAME_IS_EXIST);
-          return helper.addExistProjectToSFDXProject(projectName, body.packageName, log);
+      .then(() => {
+        if (process.env.MODE !== 'TEST') {
+          log.log('Dev Mode');
+          return devMode(body, projectName, resBody, log);
         }
-        log.log('SFDX Unlocked Package Created');
-        log.log(result.stdout);
-        return Promise.resolve();
-      })
-      .then(() => helper.callChildProcess(
-        constants.getSFDXCreateUnlockedPackageVersion(body.packageName, body.username, body.versionKey, body.versionName, body.description, body.versionNumber),
-        log,
-        { cwd: `./${projectName}`, maxBuffer: 1024 * 500 },
-      ))
-      .then((stdout) => {
-        stdout = ' Request in progress. Sleeping 30 seconds. Will wait a total of 600 more seconds before timing out. Current Status=\'Initializing\'\n Request in progress. Sleeping 30 seconds. Will wait a total of 570 more seconds before timing out. Current Status=\'Verifying metadata\'\n sfdx-project.json has been updated.\n Successfully created the package version [08c2w000000k9zUAAQ]. Subscriber Package Version Id: 04t2w000009F4ILAA0\n Package Installation URL: https://login.salesforce.com/packaging/installPackage.apexp?p0=04t2w000009F4ILAA0\n As an alternative, you can use the "sfdx force:package:install" command.';
-        log.log('SFDX Unlocked Package Version Created');
-        log.log(stdout);
-        return helper.getInstallationURL(stdout);
-      })
-      .then((installationURL) => {
-        resBody.installationURL = installationURL;
+        log.log('Test Mode');
         return Promise.resolve();
       })
       .then(() => helper.getSFDXProject(projectName))
